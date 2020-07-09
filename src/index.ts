38620ee97f0fc839499c99b459977c10d7971af6
Yadir Hernandez Batista <yadirhb@gmail.com>
2020-07-09 09:53:14 -0500

    Fixing Hasher export issue.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as alg from './algorithms';
 import * as enc from './encoders';
-import { Hasher } from 'lib/Hasher';
+import { Hasher } from './lib/Hasher';
 
 const HmacSHA256 = Hasher._createHmacHelper(alg.SHA256);
 export default { alg, enc, HmacSHA256 };
